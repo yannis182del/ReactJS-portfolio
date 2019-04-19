e1ba136a637e2ec89ae859a9aa0c957128cbbfb6
Yannis <Yannis182@outlook.fr>
2019-04-19 15:17:03 +0200

    removed image

--- src/ui elements/Header.js
+++ src/ui elements/Header.js
@@ -1,12 +1,12 @@
 import React from "react"
 import '../css/Header.css';
-import logo from "/Users/yannisbouacida/Desktop/react-portfolio/my-app/src/IMG_3961.jpg";
+// import logo from "/Users/yannisbouacida/Desktop/react-portfolio/my-app/src/IMG_3961.jpg";
 
 const Header = () => {
     return(
    <div className="Header-container">
     <div className="Picture-container">
-      <img className="My-picture" src={logo} alt="Logo" />
+      {/* <img className="My-picture" src={logo} alt="Logo" /> */}
     </div>
    <h1>Yannis Bouacida</h1>
    <h2>Software developer</h2>
